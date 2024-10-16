fb047fcd404fdebb03afe578a2eb7c703d54a424
Tao Su <152714152+tomstao@users.noreply.github.com>
2024-10-16 10:48:09 -0400

    Revert "Imlegend ver0.1"

--- textContent.cpp
+++ textContent.cpp
@@ -3,11 +3,10 @@
 //
 
 #include "textContent.h"
-#include "gameFuntions.h"
+using namespace std;
 #include <iostream>
 
 void printIntroduction() {
-    using namespace std;
     cout << "You wakes up in an abandoned city, overgrown with nature." << endl;
     cout << "it’s been years since a catastrophic event--" << endl;
     cout << "turned the population into bloodthirsty creatures." <<endl;
@@ -18,7 +17,6 @@ void printIntroduction() {
 
 void printIntroduction2()
 {
-    using namespace std;
     cout << "The world as you knew it ended long ago. "<< endl;
     cout << "Civilization collapsed after a virus turned most of the population into night-dwelling monsters. " << endl;
     cout << "You are one of the last humans, alone in a vast, empty city, " <<endl;
@@ -27,22 +25,13 @@ void printIntroduction2()
 
 void dayExploration()
 {
-    using namespace std;
     cout << "The player can explore various city locations: " << endl;
     cout << "an old lab, a library, abandoned houses, and barricaded stores. "<< endl;
-    cout << "Each place holds clues, supplies, or remnants of humanity."<< endl;
+    cout << "Each place holds clues, supplies, or remnants of humanity. "<< endl;
     cout << "There are no enemies during the day, but there is always a sense of danger."<< endl;
 }
 
 void dayOne()
 {
-    using namespace std;
-    cout << "DAY 1 : " << endl;
-    cout << "Scene Description:" << endl;
-    cout << "You awaken in your dilapidated safehouse," << endl;
-    cout << "surrounded by remnants of a once-bustling city." << endl;
-    cout << "Sunlight spills through the broken windows," << endl;
-    cout << "giving you a moment of peace before the danger returns." << endl;
-    nextNarration();
 
 }
